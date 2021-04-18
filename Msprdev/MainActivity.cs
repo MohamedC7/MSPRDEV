6e560b906d5a10c8e0f29c258843491079390a02
Mohamed CHAIEB <mohamed.chaieb@coservit.fr>
2021-04-19 01:43:46 +0200

    last commit for the beta version

--- Msprdev/MainActivity.cs
+++ Msprdev/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -20,6 +21,16 @@ using Google.Android.Material.Snackbar;
 using System.Text;
 using IronPython.Hosting;
 using Microsoft.Scripting.Hosting;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Xamarin.Forms;
+using static Android.Graphics.ImageDecoder;
+using View = Android.Views.View;
+using Bitmap = System.Drawing.Bitmap;
+using Image = Xamarin.Forms.Image;
+using System.Threading.Tasks;
+using Xamarin.Forms.Platform.Android;
+using Android.Util;
 
 
 
@@ -85,9 +96,8 @@ namespace Msprdev
 
         private void CaptureButton_Click(object sender, System.EventArgs e)
         {
-           
-            
-           TakePhoto();
+
+            TakePhoto();
             
         }
 
@@ -112,34 +122,140 @@ namespace Msprdev
 
             // Convert file to byte array and set the resulting bitmap to imageview
             byte[] imageArray = System.IO.File.ReadAllBytes(file.Path);
-            Bitmap bitmap = BitmapFactory.DecodeByteArray(imageArray, 0, imageArray.Length);
+            Android.Graphics.Bitmap bitmap = BitmapFactory.DecodeByteArray(imageArray, 0, imageArray.Length);
             thisImageView.SetImageBitmap(bitmap);
-            Option2_IronPython();
+            Console.WriteLine(Compare(bitmap, bitmap));
 
-
-
+            ImageProcessing(bitmap);
+            
         }
 
 
-
-
-        static void Option2_IronPython()
+        public static Android.Graphics.Bitmap ImageProcessing(Android.Graphics.Bitmap source)
         {
-            var engine = Python.CreateEngine();
+            
             try
             {
-                engine.ExecuteFile("D:\\check_if_two_images_are_equal.py");
+
+                DirectoryInfo dir = new DirectoryInfo(@"D:\MSPRDEV\Msprdev\model\2D\");
+                FileInfo[] imageFiles = dir.GetFiles("*.png");
+                Console.WriteLine("Found {0} *.png files\n", imageFiles.Length);
+                bool found = false;
+
+                foreach (FileInfo img in imageFiles)
+                {
+                    found = IronPython1(img.ToString(), source.ToString());
+                    if (found)
+                    {
+                        dir = new DirectoryInfo(@"D:\MSPRDEV\Msprdev\model\3D\");
+                        imageFiles = dir.GetFiles(img + "*3D.png");
+                        // Get the 3D model for the 2D model
+                        
+                        break;
+                    }
+
+                }
+
+                // detect the  color 
+                var current3D = imageFiles[0];
+                System.Drawing.Color colorofobject = IronPython2(current3D.ToString());
+
+                // set color to the 3D model
+                Android.Graphics.Bitmap bitmap = IronPython3(current3D.ToString(), colorofobject);
+                return bitmap;              
+
             }
-            catch(Exception exp)
+
+
+
+            catch (Exception exp)
             {
-                Console.WriteLine(exp);
+
             }
-            Console.ReadLine();
-            /*
+           
+            // return the 3D image in the view
+            return source;
+
+
+        }
+        static string PathAddBackslash(string path)
+        {
+            // They're always one character but EndsWith is shorter than
+            // array style access to last path character. Change this
+            // if performance are a (measured) issue.
+            string separator1 = System.IO.Path.DirectorySeparatorChar.ToString();
+            string separator2 = System.IO.Path.AltDirectorySeparatorChar.ToString();
+
+            // Trailing white spaces are always ignored but folders may have
+            // leading spaces. It's unusual but it may happen. If it's an issue
+            // then just replace TrimEnd() with Trim(). Tnx Paul Groke to point this out.
+            path = path.TrimEnd();
+
+            // Argument is always a directory name then if there is one
+            // of allowed separators then I have nothing to do.
+            if (path.EndsWith(separator1) || path.EndsWith(separator2))
+                return path;
+
+            // If there is the "alt" separator then I add a trailing one.
+            // Note that URI format (file://drive:\path\filename.ext) is
+            // not supported in most .NET I/O functions then we don't support it
+            // here too. If you have to then simply revert this check:
+            // if (path.Contains(separator1))
+            //     return path + separator1;
+            //
+            // return path + separator2;
+            if (path.Contains(separator2))
+                return path + separator2;
+
+            // If there is not an "alt" separator I add a "normal" one.
+            // It means path may be with normal one or it has not any separator
+            // (for example if it's just a directory name). In this case I
+            // default to normal as users expect.
+            return path + separator1;
+        }
+        public static bool Compare(Android.Graphics.Bitmap img1, Android.Graphics.Bitmap img2)
+        {
+            string img1_ref, img2_ref;
+            bool equal = true;
+            int count1 =1;
+            int count2 = 0;
+            bool flag = true;
+
+            if (img1.Width == img2.Width && img1.Height == img2.Height)
+            {
+                for (int i = 0; i < img1.Width; i++)
+                {
+                    for (int j = 0; j < img1.Height; j++)
+                    {
+                        img1_ref = img1.GetPixel(i, j).ToString();
+                        img2_ref = img2.GetPixel(i, j).ToString();
+                        if (img1_ref != img2_ref)
+                        {
+                            count2++;
+                            flag = false;
+                            break;
+                        }
+                        count1++;
+                    }
+
+                }
+                if (flag == false)
+                    Console.WriteLine("Sorry, Images are not same , " + count2 + " wrong pixels found");
+                else
+                    Console.WriteLine(" Images are same , " + count1 + " same pixels found and " + count2 + " wrong pixels found");
+            }
+            else
+                Console.WriteLine("can not compare this images");
+        return flag;
+                    }
+
+
+
+        static bool IronPython1(string img1, string img2)
+        {                    
               //instance of python engine
               var engine = Python.CreateEngine();
               //reading code from file
-
               var source = engine.CreateScriptSourceFromFile(System.IO.Path.Combine("D:\\MSPRDEV\\Msprdev\\", "check_if_two_images_are_equal.py"));
               var scope = engine.CreateScope();
               //executing script in scope
@@ -148,75 +264,41 @@ namespace Msprdev
               //initializing class
               var ImageProcessInstance = engine.Operations.CreateInstance(classImageProcess);
               Console.WriteLine("From Iron Python");
-              Console.WriteLine("5 + 10 = {0}", ImageProcessInstance.equal("Aipom.png", "Ekans.png"));
+              return ImageProcessInstance.equal(img1, img2);
 
-              */
+        }
 
+        static System.Drawing.Color IronPython2(string img)
+        {
+            //instance of python engine
+            var engine = Python.CreateEngine();
+            //reading code from file
+            var source = engine.CreateScriptSourceFromFile(System.IO.Path.Combine("D:\\MSPRDEV\\Msprdev\\", "detect_color.py"));
+            var scope = engine.CreateScope();
+            //executing script in scope
+            source.Execute(scope);
+            var DetectColor = scope.GetVariable("DetectColor");
+            //initializing class
+            var ImageProcessInstance = engine.Operations.CreateInstance(DetectColor);
+            Console.WriteLine("From Iron Python");
+            return ImageProcessInstance.detectcolor(img);
 
+        }
 
-            /*
-            var engine = Python.CreateEngine(); // Extract Python language engine from their grasp
-            var scope = engine.CreateScope(); // Introduce Python namespace (scope)
-
-
-            var argv = new List<string>();
-            argv.Add("");
-            argv.Add("Aipom.png");
-            argv.Add("Ekans");
-
-            engine.GetSysModule().SetVariable("argv", argv);// Add some sample parameters. Notice that there is no need in specifically setting the object type, interpreter will do that part for us in the script properly with high probability
-
-           
-            ScriptSource source = engine.CreateScriptSourceFromFile("D:\\MSPRDEV\\Msprdev\\check_if_two_images_are_equals.py"); // Load the script
-            object result = source.Execute(scope);
-            */
-            /*
-
-
-
-
-
-
-              // 1) Create engine
-              var engine = Python.CreateEngine();
-
-              // 2) Provide script and arguments
-              var script = @"check_if_two_images_are_equals.py";                   
-              var source = engine.CreateScriptSourceFromFile(script);
-
-              var argv = new List<string>();
-              argv.Add("");
-              argv.Add("Aipom.png");
-              argv.Add("Ekans");
-
-              engine.GetSysModule().SetVariable("argv", argv);
-
-              // 3) Output redirect
-              var eIO = engine.Runtime.IO;
-
-              var errors = new MemoryStream();
-              eIO.SetErrorOutput(errors, Encoding.Default);
-
-              var results = new MemoryStream();
-              eIO.SetOutput(results, Encoding.Default);
-
-              // 4) Execute script
-              ScriptScope scope = engine.CreateScope();
-              string expr = "equal('Aipom.png','Ekans.png')";
-              var result = engine.Execute(expr, scope);
-              //var scope = engine.CreateScope();
-             // source.Execute(scope);
-
-
-              // 5) Display output
-              string str(byte[] x) => Encoding.Default.GetString(x);
-
-              Console.WriteLine("ERRORS:");
-              Console.WriteLine(str(errors.ToArray()));
-              Console.WriteLine();
-              Console.WriteLine("Results:");
-              Console.WriteLine(str(results.ToArray()));
-              */
+        static Android.Graphics.Bitmap IronPython3(string objectC, System.Drawing.Color color)
+        {
+            //instance of python engine
+            var engine = Python.CreateEngine();
+            //reading code from file
+            var source = engine.CreateScriptSourceFromFile(System.IO.Path.Combine("D:\\MSPRDEV\\Msprdev\\", "color_3D_model.py"));
+            var scope = engine.CreateScope();
+            //executing script in scope
+            source.Execute(scope);
+            var SetColor = scope.GetVariable("Color3D");
+            //initializing class
+            var ImageProcessInstance = engine.Operations.CreateInstance(SetColor);
+            Console.WriteLine("From Iron Python");
+            return ImageProcessInstance.setColor(objectC, color);
 
         }
 
@@ -241,7 +323,7 @@ namespace Msprdev
             // Convert file to byre array, to bitmap and set it to our ImageView
 
             byte[] imageArray = System.IO.File.ReadAllBytes(file.Path);
-            Bitmap bitmap = BitmapFactory.DecodeByteArray(imageArray, 0, imageArray.Length);
+            Android.Graphics.Bitmap bitmap = BitmapFactory.DecodeByteArray(imageArray, 0, imageArray.Length);
             thisImageView.SetImageBitmap(bitmap);
 
         }
@@ -280,7 +362,7 @@ namespace Msprdev
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
             View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+            Snackbar.Make(view, "Share on social media", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
 
@@ -325,39 +407,6 @@ namespace Msprdev
         }
 
 
-        /*
-        public void ImageProcessing()
-        {
-            {
-                Microsoft.Scripting.Hosting.ScriptEngine pythonEngine =
-                    IronPython.Hosting.Python.CreateEngine();
-                // We execute this script from Visual Studio 
-                // so the program will be executed from bin\Debug or bin\Release
-                Microsoft.Scripting.Hosting.ScriptSource pythonScript =
-                    pythonEngine.CreateScriptSourceFromFile("C:\\Users\\mchaieb\\source\\repos\\Msprdev\\Msprdev\\check_if_two_images_are_equal.py");
-                pythonScript.Execute();
-            }
-        }
-        */
-
-        /*
-        public string PatchParameter(string parameter, int serviceid)
-        {
-            var engine = Python.CreateEngine(); // Extract Python language engine from their grasp
-            var scope = engine.CreateScope(); // Introduce Python namespace (scope)
-            var d = new Dictionary<string, object>
-            {
-                { "serviceid", serviceid},
-                { "parameter", parameter}
-            }; // Add some sample parameters. Notice that there is no need in specifically setting the object type, interpreter will do that part for us in the script properly with high probability
-
-            scope.SetVariable("params", d); // This will be the name of the dictionary in python script, initialized with previously created .NET Dictionary
-            ScriptSource source = engine.CreateScriptSourceFromFile("check_if_two_images_are_equal.py"); // Load the script
-            object result = source.Execute(scope);
-            parameter = scope.GetVariable<string>("parameter"); // To get the finally set variable 'parameter' from the python script
-            return parameter;
-        }
-        */
 
 
 
--- Msprdev/color_3D_model.py
+++ Msprdev/color_3D_model.py
@@ -2,13 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-def setColor(img):
-    nemo = cv2.imread('./model3D/nemo.jpg')
-    plt.imshow(nemo)
-    plt.show()
 
-    nemo = cv2.cvtColor(nemo, cv2.COLOR_BGR2RGB)
-    plt.imshow(nemo)
-    plt.show()
-    return nemo
+class Color3D:
+
+    def setColor(img,color):
+        nemo = cv2.imread('./model3D/nemo.jpg')
+        plt.imshow(nemo)
+        plt.show()
+
+        nemo = cv2.cvtColor(nemo, cv2.COLOR_BGR2RGB)
+        plt.imshow(nemo)
+        plt.show()
+        return nemo
 
